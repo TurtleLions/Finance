fdf10f8f7bab7f559b75086e3c079b1f02b5bc82
Ryan <ryanestanislao@gmail.com>
2023-05-01 09:11:17 -0700

    postsearch

---
+++ app/src/main/java/com/example/finance/Post_Search.kt
@@ -0,0 +1,12 @@
+package com.example.finance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Post_Search : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_postsearch)
+
+    }
+}
