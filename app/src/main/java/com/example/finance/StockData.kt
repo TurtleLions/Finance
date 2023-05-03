13af710bdddc98a37f8cf42223d0461027d379c0
Ryan <ryanestanislao@gmail.com>
2023-05-03 10:45:33 -0700

    push

--- app/src/main/java/com/example/finance/StockData.kt
+++ app/src/main/java/com/example/finance/StockData.kt
@@ -1,5 +1,30 @@
 package com.example.finance
 
-data class StockData() {
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
+data class StockData(
+    @SerializedName("Meta Data")
+    val metadata:MetaData,
+    @SerializedName("Time Series (5min)")
+    val fiveMinTimeSeries : Map<String, >?
+
+):Parcelable{
+    @Parcelize
+    data class MetaData(
+        @SerializedName("1. Information")
+        val information: String,
+        @SerializedName("2. Symbol")
+        val symbol: String,
+        @SerializedName("3. Last Refreshed")
+        val lastRefreshed: String,
+        @SerializedName("4. Interval")
+        val interval: String,
+        @SerializedName("5. Output Size")
+        val outputSize: String,
+        @SerializedName("6. Time Zone")
+        val timeZone: String
+    ):Parcelable
 }
