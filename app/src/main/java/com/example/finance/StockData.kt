1c30e28c18aaac2f657b465e3cb59bd27c9f7d65
Ryan <ryanestanislao@gmail.com>
2023-05-03 10:59:05 -0700

    bye InterStockData.kt

--- app/src/main/java/com/example/finance/StockData.kt
+++ app/src/main/java/com/example/finance/StockData.kt
@@ -9,10 +9,10 @@ data class StockData(
     @SerializedName("Meta Data")
     val metadata: Map<String, String>,
     @SerializedName("Time Series (Daily)")
-    val dailyTimeSeries: Map<String, InterStockData>?,
+    val dailyTimeSeries: Map<String, Map<String,String>>?,
     @SerializedName("Weekly Adjusted Time Series")
-    val weeklyTimeSeries: Map<String, InterStockData>?,
+    val weeklyTimeSeries: Map<String, Map<String,String>>?,
     @SerializedName("Monthly Adjusted Time Series")
-    val monthlyTimeSeries: Map<String, InterStockData>?
+    val monthlyTimeSeries: Map<String, Map<String,String>>?
 ):Parcelable{
 }
