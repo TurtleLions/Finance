40ec4d91dc6ab4c0802a929b803d0fac1ad30a1f
Ryan <ryanestanislao@gmail.com>
2023-05-03 10:22:00 -0700

    stock data

--- app/src/main/java/com/example/finance/StockDetailActivity.kt
+++ app/src/main/java/com/example/finance/StockDetailActivity.kt
@@ -2,7 +2,11 @@ package com.example.finance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.finance.databinding.ActivityStockDetailBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class StockDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStockDetailBinding
@@ -19,5 +23,25 @@ class StockDetailActivity : AppCompatActivity() {
         binding.detailName.text = stock.name
         binding.detailSymbol.text = stock.ticker
         binding.detailExchange.text = stock.exchange
+        suspend fun getStockDataByApiCall(function:String, symbol:String) {
+            val FinanceDataService = RetrofitHelper.getInstance().create(FinanceDataService::class.java)
+            val stockDataCall = FinanceDataService.getStockData(function,
+                symbol, Constants.API_KEY)
+            stockDataCall.enqueue(object: Callback<> {
+                override fun onResponse(
+                    call: Call<PlayerData>,
+                    response: Response<PlayerData>
+                ) {
+                    Log.d(TAG, "onResponse: ${response.body()}")
+                    if(response.body()?.status=="ok"){
+                        playerData = response.body()!!
+                    }
+                }
+
+                override fun onFailure(call: Call<PlayerData>, t: Throwable) {
+                    Log.d(TAG, "onFailure: ${t.message}")
+                }
+            })
+        }
     }
 }
