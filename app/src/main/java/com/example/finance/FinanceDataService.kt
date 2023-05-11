cdb9ed834d4715cfa8d88f763526a9d29b09fefb
Ryan <ryanestanislao@gmail.com>
2023-05-11 09:36:50 -0700

    z

--- app/src/main/java/com/example/finance/FinanceDataService.kt
+++ app/src/main/java/com/example/finance/FinanceDataService.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.Query
 
 interface FinanceDataService {
-    @GET("")
+    @GET("query/")
     fun getStockData(
         @Query("function") function: String,
         @Query("symbol") symbol:String,
--- app/src/main/java/com/example/finance/RetrofitHelper.kt
+++ app/src/main/java/com/example/finance/RetrofitHelper.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    var baseUrl = "https://alphadvantage.co/query"
+    var baseUrl = "https://alphadvantage.co/"
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
