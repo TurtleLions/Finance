2c16622b1a24e1b66c0712b1221c1c359826dffc
Ryan <ryanestanislao@gmail.com>
2023-05-11 08:39:22 -0700

    q

--- app/src/main/java/com/example/finance/StockDetailActivity.kt
+++ app/src/main/java/com/example/finance/StockDetailActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.finance.databinding.ActivityStockDetailBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,6 +27,11 @@ class StockDetailActivity : AppCompatActivity() {
         binding.detailName.text = stock.name
         binding.detailSymbol.text = stock.ticker
         binding.detailExchange.text = stock.exchange
+        GlobalScope.launch {
+            async {
+                getStockDataByApiCall()
+            }
+        }
 
     }
     suspend fun getStockDataByApiCall(function:String, symbol:String) {
