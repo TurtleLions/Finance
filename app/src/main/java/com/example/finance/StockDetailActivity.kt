6822dc65b35db5abb0b69e88ceb909bd214c726a
lucsrathbun <lucsrathbun@gmail.com>
2023-05-15 09:17:14 -0700

    inter

--- app/src/main/java/com/example/finance/StockDetailActivity.kt
+++ app/src/main/java/com/example/finance/StockDetailActivity.kt
@@ -10,9 +10,9 @@ import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.LineGraphSeries
+//import com.jjoe64.graphview.GraphView
+//import com.jjoe64.graphview.series.DataPoint
+//import com.jjoe64.graphview.series.LineGraphSeries
 
 class StockDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStockDetailBinding
@@ -20,7 +20,7 @@ class StockDetailActivity : AppCompatActivity() {
     private lateinit var dailyStockData:StockData
     private lateinit var weeklyStockData:StockData
     private lateinit var monthlyStockData:StockData
-    private lateinit var lineGraphView: GraphView
+//    private lateinit var lineGraphView: GraphView
     private var timeState = 0
     companion object{
         val TAG = "Stock Detail Activity"
@@ -35,7 +35,7 @@ class StockDetailActivity : AppCompatActivity() {
         binding.detailSymbol.text = stock.ticker
         binding.detailExchange.text = stock.exchange
         binding.buttonTimeGraph.text = "Daily"
-        lineGraphView = findViewById(R.id.idGraphView)
+//        lineGraphView = findViewById(R.id.idGraphView)
         binding.buttonTimeGraph.setOnClickListener {
             if(timeState==0){
                 binding.buttonTimeGraph.text = "Weekly"
@@ -64,43 +64,45 @@ class StockDetailActivity : AppCompatActivity() {
             Log.d(TAG, weeklyStockData.toString())
             Log.d(TAG, monthlyStockData.toString())
         }
-        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
-            arrayOf(
-                // on below line we are adding
-                // each point on our x and y axis.
-                DataPoint(0.0, 1.0),
-                DataPoint(1.0, 3.0),
-                DataPoint(2.0, 4.0),
-                DataPoint(3.0, 9.0),
-                DataPoint(4.0, 6.0),
-                DataPoint(5.0, 3.0),
-                DataPoint(6.0, 6.0),
-                DataPoint(7.0, 1.0),
-                DataPoint(8.0, 2.0)
-            )
-        )
+/*
+//        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
+//            arrayOf(
+//                // on below line we are adding
+//                // each point on our x and y axis.
+//                DataPoint(0.0, 1.0),
+//                DataPoint(1.0, 3.0),
+//                DataPoint(2.0, 4.0),
+//                DataPoint(3.0, 9.0),
+//                DataPoint(4.0, 6.0),
+//                DataPoint(5.0, 3.0),
+//                DataPoint(6.0, 6.0),
+//                DataPoint(7.0, 1.0),
+//                DataPoint(8.0, 2.0)
+//            )
+//        )
 
-        // on below line adding animation
-        lineGraphView.animate()
-
-        // on below line we are setting scrollable
-        // for point graph view
-        lineGraphView.viewport.isScrollable = true
-
-        // on below line we are setting scalable.
-        lineGraphView.viewport.isScalable = true
-
-        // on below line we are setting scalable y
-        lineGraphView.viewport.setScalableY(true)
-
-        // on below line we are setting scrollable y
-        lineGraphView.viewport.setScrollableY(true)
-        lineGraphView.addSeries(series)
+//        // on below line adding animation
+//        lineGraphView.animate()
+//
+//        // on below line we are setting scrollable
+//        // for point graph view
+//        lineGraphView.viewport.isScrollable = true
+//
+//        // on below line we are setting scalable.
+//        lineGraphView.viewport.isScalable = true
+//
+//        // on below line we are setting scalable y
+//        lineGraphView.viewport.setScalableY(true)
+//
+//        // on below line we are setting scrollable y
+//        lineGraphView.viewport.setScrollableY(true)
+//        lineGraphView.addSeries(series)
+*/
 
     }
     suspend fun getDailyStockDataByApiCall(function:String, symbol:String) {
         val FinanceDataService = RetrofitHelper.getInstance().create(FinanceDataService::class.java)
-        //Log.d(TAG, function +" "+symbol)
+        Log.d(TAG, function +" "+symbol)
         val stockDataCall = FinanceDataService.getDailyStockData(function,
             symbol, Constants.API_KEY)
         stockDataCall.enqueue(object: Callback<StockData> {
@@ -119,7 +121,7 @@ class StockDetailActivity : AppCompatActivity() {
     }
     suspend fun getWeeklyStockDataByApiCall(function:String, symbol:String) {
         val FinanceDataService = RetrofitHelper.getInstance().create(FinanceDataService::class.java)
-        //Log.d(TAG, function +" "+symbol)
+        Log.d(TAG, function +" "+symbol)
         val stockDataCall = FinanceDataService.getWeeklyStockData(function,
             symbol, Constants.API_KEY)
         stockDataCall.enqueue(object: Callback<StockData> {
@@ -138,7 +140,7 @@ class StockDetailActivity : AppCompatActivity() {
     }
     suspend fun getMonthStockDataByApiCall(function:String, symbol:String) {
         val FinanceDataService = RetrofitHelper.getInstance().create(FinanceDataService::class.java)
-        //Log.d(TAG, function +" "+symbol)
+        Log.d(TAG, function +" "+symbol)
         val stockDataCall = FinanceDataService.getMonthlyStockData(function,
             symbol, Constants.API_KEY)
         stockDataCall.enqueue(object: Callback<StockData> {
